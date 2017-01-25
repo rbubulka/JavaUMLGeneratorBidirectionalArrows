2363c0a6645efd93291dc4e2af1692ee2826b2aa
bubulkr <bubulkr@rose-hulman.edu>
2017-01-24 22:50:28 -0500

    make parser

--- src/main/BidirectionalArrowParser.java
+++ src/main/BidirectionalArrowParser.java
@@ -1,5 +1,18 @@
 package main;
+import java.util.List;
+import java.util.Set;
+import parsers.ClassParser.ClassParser;
+public class BidirectionalArrowParser extends ClassParser {
 
-public class BidirectionalArrowParser {
+	public BidirectionalArrowParser(ClassParser other) {
+		super(other);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String parse(List nodes, Set<String> relations) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
